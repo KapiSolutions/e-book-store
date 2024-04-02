c8436c0e322c7f45b6f3538f829fadbb1f461395
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-02 10:50:42 +0200

    init commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,44 @@
+<script>
+	import landing from '$lib/images/landing.png';
+	import Navbar from './Navbar.svelte';
+</script>
+
+<header>
+	<Navbar />
+	<section class="landing">
+		<div>
+			<h1>Twój E-book</h1>
+			<p>
+				Poszerz swoje horyzonty z naszymi e-bookami! Unikalna wiedza w zasięgu jednego kliknięcia!
+			</p>
+			<button>Zobacz E-booki!</button>
+		</div>
+		<div>
+			<span class="landing-image">
+				<img src={landing} alt="Welcome" />
+			</span>
+		</div>
+	</section>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+	.landing{
+		display: flex;
+		justify-content: space-around;
+		width: 100%;
+	}
+
+	.landing div{
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+	.landing div p{
+		max-width: 300px;
+	}
+</style>
