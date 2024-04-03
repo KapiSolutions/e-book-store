26bba29f4d4d2bd2676ea302d655f954c1e51aab
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-03 21:58:44 +0200

    chore: create footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,65 @@
+<script>
+	import emailIcon from '$lib/images/emailIcon.svg';
+	import phoneIcon from '$lib/images/phoneIcon.svg';
+	import pinIcon from '$lib/images/pinIcon.svg';
+</script>
+
+<footer id="footer">
+	<div class="container">
+		<p><strong>Kontakt</strong></p>
+		<div>
+			<img src={emailIcon} alt="email" />
+			<p><a href="mailto:twojebook10@gmail.com" class="nodecoration">twojebook10@gmail.com</a></p>
+		</div>
+		<div>
+			<img src={phoneIcon} alt="phone" />
+			<p>+48 572 217 369</p>
+		</div>
+		<div>
+			<img src={pinIcon} alt="phone" />
+			<p>ul. Krasickiego 48A, 32-600 Oświęcim</p>
+		</div>
+	</div>
+	<p>© 2024 <a href="/#">Twój E-book</a></p>
+</footer>
+
+<style>
+	.container {
+		width: 80%;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		border-top: 1px solid rgba(234, 234, 234, 0.188);
+	}
+	.container div {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		text-align: center;
+	}
+	.nodecoration {
+		text-decoration: none;
+		color: var(--color-text);
+	}
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+		background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 100%);
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+		.container div {
+			display: flex;
+			align-items: center;
+			flex-direction: row;
+			text-align: center;
+			gap: 6px;
+		}
+	}
+</style>
