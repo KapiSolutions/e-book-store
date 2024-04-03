6b0a663377df991ff4848b42e43037093a279858
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-03 11:57:31 +0200

    chore: create header text component

---
+++ src/lib/components/HeaderText.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let title = 'Section title';
+</script>
+
+<div class="container">
+	<h1>{title}</h1>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+	}
+
+	h1 {
+		text-transform: uppercase;
+	}
+</style>
