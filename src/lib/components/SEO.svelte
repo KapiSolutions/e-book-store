41e4e98f5cfa1b7bf655ee33414f4e97e23b6562
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-03 11:58:39 +0200

    chore: create SEO component

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let title: string = 'Twój E-book!';
+	export let description: string = 'Sklep z e-bookami. Unikalna wiedza w zasięgu jednego kliknięcia!';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+</svelte:head>
