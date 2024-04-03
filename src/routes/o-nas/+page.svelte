e7b3278a0ed37116585a3788b39ae1267a966d83
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-03 20:06:57 +0200

    chore: create about us page

---
+++ src/routes/o-nas/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import HeaderText from '$lib/components/HeaderText.svelte';
+	import SEO from '$lib/components/SEO.svelte';
+</script>
+
+<SEO title={'O nas'} description={'Kilka słów o nas.'} />
+
+<div class="text-column">
+	<HeaderText title="O nas" />
+
+	<p class="paragraph">Witaj w "Twój E-book!"</p>
+	<p>
+		Jesteśmy pasjonatami rozwoju osobistego, zdrowego stylu życia oraz budowania harmonijnych
+		relacji międzyludzkich. Wierzymy, że każdy z nas ma ogromny potencjał do samorealizacji i
+		osiągania pełni szczęścia.
+	</p>
+	<p>
+		Platforma "Twój E-book" powstała z myślą o tych, którzy pragną rozwijać się w różnych sferach
+		życia. Nasza oferta e-booków obejmuje zakres tematyczny dotyczący rozwoju osobistego, radzenia
+		sobie ze stresem, zdrowego odżywiania, pielęgnacji urody, medytacji, psychologii pozytywności,
+		oraz wielu innych obszarów, które mogą przyczynić się do osiągnięcia równowagi i satysfakcji w
+		życiu codziennym.
+	</p>
+	<p>
+		W "Twój E-book" znajdziesz inspirujące treści, które pomogą Ci w odkrywaniu własnych możliwości,
+		pokonywaniu przeszkód oraz stawianiu kolejnych kroków na drodze do spełnienia swoich marzeń i
+		celów. Nasze e-booki zostały starannie wyselekcjonowane, aby zapewnić Ci wartościową wiedzę,
+		praktyczne wskazówki oraz motywację do działania.
+	</p>
+	<p>
+		Zaufaj nam i dołącz do społeczności osób, które pragną zmieniać swoje życie na lepsze!
+		Zapraszamy do eksplorowania oferty e-booków oraz korzystania z naszych inspirujących treści.
+	</p>
+	<p>Dziękujemy, że jesteś z nami!</p>
+	<p><strong><i>~ Zespół Twój E-book</i></strong></p>
+</div>
+
+<style>
+	.paragraph {
+		text-align: center;
+		font-weight: bold;
+		font-size: large;
+	}
+</style>
