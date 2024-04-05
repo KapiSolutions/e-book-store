68ecc6bf3ff01b856f5788d3cef74140b0252527
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-05 16:03:40 +0200

    chore: define vercel adapter

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -1,13 +1,12 @@
 import { json } from '@sveltejs/kit';
-import * as fs from 'fs';
-import path from 'path';
+import {read} from '$app/server';
+import watermark from '$lib/assets/watermark-1.pdf'
 
 export async function GET() {
+    // return json({ message: 'Works!'});
 	try {
-		const filepath = path.join(process.cwd(), '../../../static/watermarks/watermark-1.pdf');
-        console.log(filepath);
-		fs.readFileSync(filepath);
-		return json({ message: 'Works!' });
+		const fontData = read(watermark).arrayBuffer();
+		return json({ message: 'Works!'});
 	} catch (error) {
 		return json({ error: error }, { status: 500 });
 	}
