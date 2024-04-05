5bd58018bd83f969bab91cefa9d9838ff05f5fca
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-05 15:52:55 +0200

    chore: update test route

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -1,9 +1,11 @@
 import { json } from '@sveltejs/kit';
 import * as fs from 'fs';
+import path from 'path';
 
 export async function GET() {
 	try {
-		const filepath = `${process.cwd()}/watermarks/watermark-1.pdf`;
+		const filepath = path.join(process.cwd(), '../../../static/watermarks/watermark-1.pdf');
+        console.log(filepath);
 		fs.readFileSync(filepath);
 		return json({ message: 'Works!' });
 	} catch (error) {
