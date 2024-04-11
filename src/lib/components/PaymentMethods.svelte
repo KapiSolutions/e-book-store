f3a93bcbd4db4c277c43f96baefb5d9675ce733a
JKapturkiewicz <kuba.kapek@gmail.com>
2024-04-11 19:07:31 +0200

    chore: create PaymentMethods component

---
+++ src/lib/components/PaymentMethods.svelte
@@ -0,0 +1,25 @@
+<script>
+	import blik from '$lib/assets/images/paymentMethods/blik.svg';
+	import p24 from '$lib/assets/images/paymentMethods/p24.svg';
+	import klarna from '$lib/assets/images/paymentMethods/klarna.svg';
+	import visa from '$lib/assets/images/paymentMethods/visa.svg';
+	import mastercard from '$lib/assets/images/paymentMethods/mastercard.svg';
+</script>
+
+<div>
+	<img src={mastercard} alt="mastercard" />
+	<img src={visa} alt="visa" />
+	<img src={blik} alt="Blik" />
+	<img src={p24} alt="p24" />
+	<img src={klarna} alt="klarna" />
+</div>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 12px;
+		width: 100%;
+	}
+</style>
