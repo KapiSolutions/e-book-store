d858c7a3d5c7337c13f44d4c40ab71dc296f5eeb
JKapturkiewicz <kuba.kapek@gmail.com>
2024-05-19 14:04:00 +0200

    chore: add order info

---
+++ src/lib/components/OrderInfo.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let hide: boolean = false;
+</script>
+
+<p class:hide>
+	<small>ZAKUP JEST JEDNOZNACZNY Z AKCEPTACJĄ <a href="/regulamin#main">REGULAMINU.</a></small>
+</p>
+
+<style>
+	p {
+		margin-top: 32px;
+        text-align: center;
+	}
+	.hide {
+		display: none;
+	}
+	@media screen and (max-width: 600px) {
+		p {
+			margin-top: 16px;
+		}
+	}
+</style>
